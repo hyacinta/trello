bf4045a8363d19efe71e2bec8fda1a0f5a5c9a56
hyacinta <kimkyuree84@gmail.com>
2020-05-09 16:21:12 +0900

    login01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  inputIdChange = () => {
+    console.log('사용자 아이디 입력중');
+  };
+
+  inputPwChange = () => {
+    console.log('사용자 비밀번호 입력중');
+  };
+
+  buttonClick = (e) => {
+    e.preventDefault();
+    console.log('로그인 시도');
+  };
+
+  render() {
+    return (
+      <div className="wrap">
+        <form className="loginForm">
+          <h1>로그인</h1>
+          <ul>
+            <li>
+              <label htmlFor="inputId">
+                <span className="inputLabel">ID</span>
+                <input
+                  id="inputId"
+                  type="text"
+                  placeholder="아이디를 입력하세요"
+                  onChange={this.inputIdChange}
+                  className="inputText"
+                />
+              </label>
+            </li>
+            <li>
+              <label htmlFor="inputPw">
+                <span className="inputLabel">PASSWORD</span>
+                <input
+                  id="inputPw"
+                  type="password"
+                  placeholder="비밀번호를 입력하세요"
+                  onChange={this.inputPwChange}
+                  className="inputText"
+                />
+              </label>
+            </li>
+          </ul>
+          <button type="button" className="btnLogin" onClick={this.buttonClick}>
+            로그인하기
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
